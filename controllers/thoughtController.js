4ad639946f5c24bcfa4a45a2de91a3c65f9753ab
Noah Burton <nburton115@gmail.com>
2023-10-21 13:28:51 -0600

    get thought by id

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -10,4 +10,19 @@ async function getThoughts (req, res) {
     }
 };
 
-module.exports = {getThoughts};
+async function getOneThought(req,res) {
+    try {
+        const thought = await Thought.findOne({ _id: req.params.thoughtId}) //! where is the req body defined?
+
+        if (!thought) {
+            return res.status(404).json({message : 'No thoughts with that Id...'});
+
+        }
+
+        res.json(thought);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+
+module.exports = {getThoughts, getOneThought};
